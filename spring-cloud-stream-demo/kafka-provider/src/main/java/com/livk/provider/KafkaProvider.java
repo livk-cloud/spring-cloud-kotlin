ca42c7a6ecc2e0adcb338e655db0be23b394fa57
livk1375632510 <chenyu1999>
2022-03-22 11:23:32 +0800

    feat

--- spring-cloud-stream-demo/kafka-provider/src/main/java/com/livk/provider/KafkaProvider.java
+++ spring-cloud-stream-demo/kafka-provider/src/main/java/com/livk/provider/KafkaProvider.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
 
