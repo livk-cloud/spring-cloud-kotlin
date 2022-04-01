b14c09348bc4ee20b95a7471a99479f53ed65429
livk1375632510 <chenyu1999>
2022-04-01 14:38:30 +0800

    feat:

--- spring-cloud-openfeign-demo/feign-provider/feign-provider-api/src/main/java/com/livk/provider/api/feign/factory/UserRemoteServiceFallbackFactory.java
+++ spring-cloud-openfeign-demo/feign-provider/feign-provider-api/src/main/java/com/livk/provider/api/feign/factory/UserRemoteServiceFallbackFactory.java
@@ -1,9 +1,13 @@
 package com.livk.provider.api.feign.factory;
 
+import com.livk.provider.api.domain.Users;
 import com.livk.provider.api.feign.UserRemoteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * <p>
@@ -18,6 +22,16 @@ import org.springframework.stereotype.Component;
 public class UserRemoteServiceFallbackFactory implements FallbackFactory<UserRemoteService> {
     @Override
     public UserRemoteService create(Throwable cause) {
-        return null;
+        return new UserRemoteService() {
+            @Override
+            public List<Users> users() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public Boolean save(Users users) {
+                return false;
+            }
+        };
     }
 }
