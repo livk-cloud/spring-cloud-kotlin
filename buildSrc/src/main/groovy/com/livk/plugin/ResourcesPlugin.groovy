e0c66b357ce3381136444dac4de670d93abb0726
livk <1375632510@qq.com>
2022-05-28 09:53:29 +0800

    feat

--- buildSrc/src/main/groovy/com/livk/plugin/ResourcesPlugin.groovy
+++ buildSrc/src/main/groovy/com/livk/plugin/ResourcesPlugin.groovy
@@ -16,21 +16,28 @@ import org.gradle.api.tasks.compile.JavaCompile
  */
 class ResourcesPlugin implements Plugin<Project> {
 
-    private static final List<String> COMPILER_ARGS;
+    private static final List<String> COMPILER_ARGS = new ArrayList<>()
+    private static final String UTF_8 = "UTF-8"
+    private static final String SPRING_BOOT_PLUGIN_ID = "org.springframework.boot"
 
     static {
-        COMPILER_ARGS = new ArrayList<>();
-        COMPILER_ARGS.addAll(Arrays.asList(
-                "-Xlint:-options", "-Xlint:rawtypes",
-                "-Xlint:deprecation", "-Xlint:unchecked",));
+        COMPILER_ARGS.addAll(Arrays.asList("-Xlint:-options",
+                "-Xlint:rawtypes",
+                "-Xlint:deprecation",
+                "-Xlint:unchecked",))
     }
 
     @Override
     void apply(Project project) {
-        def javaCompile = project.tasks
-                .getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME)
-                .dependsOn(Format.NAME, JavaPlugin.PROCESS_RESOURCES_TASK_NAME) as JavaCompile
-        javaCompile.getOptions().setCompilerArgs(COMPILER_ARGS)
-        javaCompile.getOptions().setEncoding("UTF-8")
+        if (project.buildFile.exists()) {
+            def javaCompile = project.tasks
+                    .getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME)
+                    .dependsOn(Format.NAME) as JavaCompile
+            if (!project.plugins.hasPlugin(SPRING_BOOT_PLUGIN_ID)) {
+                javaCompile.dependsOn(JavaPlugin.PROCESS_RESOURCES_TASK_NAME)
+            }
+            javaCompile.options.compilerArgs.addAll(COMPILER_ARGS)
+            javaCompile.options.encoding = UTF_8
+        }
     }
 }
