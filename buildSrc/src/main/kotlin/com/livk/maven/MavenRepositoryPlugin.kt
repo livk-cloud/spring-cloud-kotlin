19f2a5a6025598406d2c6c2d323331670e72e0b0
livk <1375632510@qq.com>
2022-07-11 11:52:17 +0800

    feat: kts切换

--- buildSrc/src/main/kotlin/com/livk/maven/MavenRepositoryPlugin.kt
+++ buildSrc/src/main/kotlin/com/livk/maven/MavenRepositoryPlugin.kt
@@ -17,6 +17,14 @@ abstract class MavenRepositoryPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.pluginManager.apply(MavenPublishPlugin::class.java)
         val publishing = project.extensions.getByType(PublishingExtension::class.java)
-        publishing.repositories.mavenLocal()
+        publishing.repositories.maven { maven ->
+            val releasesRepoUrl = "https:xxxx"
+            val snapshotsRepoUrl = "https://xxx"
+            maven.setUrl(if (project.version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            maven.credentials {
+                it.username = "xxxxxx"
+                it.password = "xxxxxx"
+            }
+        }
     }
 }
