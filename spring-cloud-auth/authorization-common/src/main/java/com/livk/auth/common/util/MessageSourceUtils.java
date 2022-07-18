103a6aed891ae1e10356b1a6dd63d04ed267f5b1
livk <1375632510@qq.com>
2022-07-18 16:19:30 +0800

    feat

---
+++ spring-cloud-auth/authorization-common/src/main/java/com/livk/auth/common/util/MessageSourceUtils.java
@@ -0,0 +1,26 @@
+package com.livk.auth.common.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+
+/**
+ * <p>
+ * MessageSourceUtils
+ * </p>
+ *
+ * @author livk
+ * @date 2022/7/18
+ */
+@UtilityClass
+public class MessageSourceUtils {
+
+    public MessageSource get() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.addBasenames("classpath:i18n/errors/messages");
+        messageSource.setDefaultLocale(Locale.CHINA);
+        return messageSource;
+    }
+}
