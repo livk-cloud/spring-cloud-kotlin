cdc14c932b70d0cfd1d129989606ca405ba73658
livk <1375632510@qq.com>
2022-07-25 16:23:53 +0800

    feat

--- spring-cloud-auth/authorization-server/src/main/java/com/livk/auth/server/AuthorizationServer.java
+++ spring-cloud-auth/authorization-server/src/main/java/com/livk/auth/server/AuthorizationServer.java
@@ -1,8 +1,14 @@
 package com.livk.auth.server;
 
 
+import com.livk.auth.common.core.principal.Oauth2User;
+import com.livk.auth.common.service.Oauth2UserDetailsService;
 import com.livk.spring.LivkSpring;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * <p>
@@ -16,5 +22,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthorizationServer {
     public static void main(String[] args) {
         LivkSpring.run(AuthorizationServer.class, args);
+    }
+
+
+    Oauth2User oauth2User() {
+        return new Oauth2User(123L, "livk", "123456", "18664967020",
+                true, true, true, true,
+                AuthorityUtils.createAuthorityList("USER"));
+    }
+
+    @Bean
+    public Oauth2UserDetailsService users() {
+        return new Oauth2UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                if ("livk".equals(username)) {
+                    return oauth2User();
+                }
+                return null;
+            }
+        };
     }
 }
--- spring-cloud-auth/authorization-server/src/main/java/com/livk/auth/server/config/JdbcConfig.java
+++ spring-cloud-auth/authorization-server/src/main/java/com/livk/auth/server/config/JdbcConfig.java
@@ -31,6 +31,7 @@ public class JdbcConfig {
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(AuthorizationGrantType.PASSWORD)
                 .redirectUri("http://127.0.0.1:8080/login/oauth2/code/livk-client-oidc")
                 .redirectUri("http://127.0.0.1:8080/authorized").scope(OidcScopes.OPENID).scope("livk.read")
                 .scope("livk.write").clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
