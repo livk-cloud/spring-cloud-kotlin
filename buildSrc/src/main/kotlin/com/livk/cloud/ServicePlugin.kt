c146321d9be1a72d206a64d83b35ef726983d96c
livk <1375632510@qq.com>
2022-08-11 16:32:10 +0800

    feat

--- buildSrc/src/main/kotlin/com/livk/cloud/ServicePlugin.kt
+++ buildSrc/src/main/kotlin/com/livk/cloud/ServicePlugin.kt
@@ -1,8 +1,6 @@
 package com.livk.cloud
 
-import com.livk.cloud.compile.CompileArgsPlugin
-import com.livk.cloud.info.BuildInfoPlugin
-import com.livk.cloud.dependency.ManagementPlugin
+import com.livk.cloud.info.BootPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
@@ -19,6 +17,6 @@ class ServicePlugin :Plugin<Project> {
     override fun apply(project: Project) {
         project.pluginManager.apply(JavaPlugin::class.java)
         project.pluginManager.apply(ModulePlugin::class.java)
-        project.pluginManager.apply(BuildInfoPlugin::class.java)
+        project.pluginManager.apply(BootPlugin::class.java)
     }
 }
