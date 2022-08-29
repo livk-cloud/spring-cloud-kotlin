d895474026d6ae539202a3149160a4a65f2531fd
livk <1375632510@qq.com>
2022-08-29 11:01:27 +0800

    feat

--- spring-cloud-gateway/livk-gateway/build.gradle.kts
+++ spring-cloud-gateway/livk-gateway/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":spring-cloud-gateway:gateway-starter"))
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("com.github.tomakehurst:wiremock:2.27.2")
 }
