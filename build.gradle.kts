b30bb58f8fb8cf14e31f9c10369983ebf25893ea
livk <1375632510@qq.com>
2022-09-21 10:16:45 +0800

    feat

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ configure(springModuleProjects) {
 
     dependencies {
         implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+        implementation("org.springframework.boot:spring-boot-devtools")
     }
 }
 
