be010b39ed2ff41385912a9c3841cc82c9b4c72e
livk <1375632510@qq.com>
2022-09-28 10:15:58 +0800

    feat

--- spring-cloud-gateway/gateway-starter/build.gradle.kts
+++ spring-cloud-gateway/gateway-starter/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
     optional("org.springframework:spring-webflux")
     api(project(":spring-common"))
     api("org.springframework.boot:spring-boot-starter-data-redis-reactive")
-    api("io.springfox:springfox-boot-starter")
     api("org.springframework.cloud:spring-cloud-commons")
     api("com.fasterxml.jackson.core:jackson-databind")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
