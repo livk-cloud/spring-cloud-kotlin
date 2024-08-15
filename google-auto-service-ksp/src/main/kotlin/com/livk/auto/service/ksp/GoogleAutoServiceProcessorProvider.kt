957471e25ad3feee0ab1f9c03fd8be08b82d24cb
livk <livk.cloud@gmail.com>
2024-08-15 14:37:02 +0800

    refactor

--- google-auto-service-ksp/src/main/kotlin/com/livk/auto/service/ksp/GoogleAutoServiceProcessorProvider.kt
+++ google-auto-service-ksp/src/main/kotlin/com/livk/auto/service/ksp/GoogleAutoServiceProcessorProvider.kt
@@ -1,6 +1,5 @@
 package com.livk.auto.service.ksp
 
-import com.google.common.collect.Sets
 import com.google.devtools.ksp.closestClassDeclaration
 import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.SymbolProcessor
@@ -8,10 +7,8 @@ import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 import com.google.devtools.ksp.symbol.KSAnnotation
 import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSFile
 import com.google.devtools.ksp.symbol.KSType
 import java.io.IOException
-import java.util.SortedSet
 
 /**
  * <p>
@@ -43,25 +40,21 @@ class GoogleAutoServiceProcessorProvider : SymbolProcessorProvider {
             if (!providers.isEmpty) {
                 for (providerInterface in providers.keySet()) {
                     val resourceFile = "META-INF/services/${providerInterface}"
-                    logger.info("Working on resource file: $resourceFile")
+                    logger.info(supportAnnotation() + " working on resource file: $resourceFile")
                     try {
-                        val allServices: SortedSet<String> = Sets.newTreeSet()
-                        val foundImplementers = providers[providerInterface]
-                        val newServices: Set<String> = HashSet(foundImplementers.map { it.first })
-                        allServices.addAll(newServices)
-                        logger.info("New service file contents: $allServices")
-                        val ksFiles = foundImplementers.map { it.second }
-                        logger.info("Originating files: ${ksFiles.map(KSFile::fileName)}")
-                        val dependencies = Dependencies(true, *ksFiles.toTypedArray())
+                        val autoService = providers[providerInterface].map { it.first }
+                        logger.info(supportAnnotation() + " file contents: $autoService")
+                        val dependencies =
+                            Dependencies(true, *providers[providerInterface].map { it.second }.toTypedArray())
                         generator.createNewFile(dependencies, "", resourceFile, "").bufferedWriter().use { writer ->
-                            for (service in allServices) {
+                            for (service in autoService) {
                                 writer.write(service)
                                 writer.newLine()
                             }
                         }
-                        logger.info("Wrote to: $resourceFile")
+                        logger.info(supportAnnotation() + " wrote to: $resourceFile")
                     } catch (e: IOException) {
-                        logger.error("Unable to create $resourceFile, $e")
+                        logger.error(supportAnnotation() + " unable to create $resourceFile, $e")
                     }
                 }
                 providers.clear()
--- spring-auto-service-ksp/src/main/kotlin/com/livk/auto/service/ksp/SpringAutoServiceProcessorProvider.kt
+++ spring-auto-service-ksp/src/main/kotlin/com/livk/auto/service/ksp/SpringAutoServiceProcessorProvider.kt
@@ -1,7 +1,6 @@
 package com.livk.auto.service.ksp
 
 import com.google.auto.service.AutoService
-import com.google.common.collect.Sets
 import com.google.devtools.ksp.closestClassDeclaration
 import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.SymbolProcessor
@@ -9,10 +8,8 @@ import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 import com.google.devtools.ksp.symbol.KSAnnotation
 import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSFile
 import com.google.devtools.ksp.symbol.KSType
 import java.io.IOException
-import java.util.SortedSet
 
 /**
  * @author livk
@@ -38,27 +35,23 @@ class SpringAutoServiceProcessorProvider : SymbolProcessorProvider {
 
         override fun generateAndClearConfigFiles() {
             if (!providers.isEmpty) {
-                for (providerInterface in providers.keySet()) {
-                    val resourceFile = "META-INF/spring/${providerInterface}.imports"
-                    logger.info("Working on resource file: $resourceFile")
+                for (annotationName in providers.keySet()) {
+                    val resourceFile = "META-INF/spring/${annotationName}.imports"
+                    logger.info(supportAnnotation() + " on resource file: $resourceFile")
                     try {
-                        val allServices: SortedSet<String> = Sets.newTreeSet()
-                        val foundImplementers = providers[providerInterface]
-                        val newServices: Set<String> = HashSet(foundImplementers.map { it.first })
-                        allServices.addAll(newServices)
-                        logger.info("New service file contents: $allServices")
-                        val ksFiles = foundImplementers.map { it.second }
-                        logger.info("Originating files: ${ksFiles.map(KSFile::fileName)}")
-                        val dependencies = Dependencies(true, *ksFiles.toTypedArray())
+                        val autoConfigurationImpls = providers[annotationName].map { it.first }
+                        logger.info(supportAnnotation() + " file contents: $autoConfigurationImpls")
+                        val ksFiles = providers[annotationName].map { it.second }.toTypedArray()
+                        val dependencies = Dependencies(true, *ksFiles)
                         generator.createNewFile(dependencies, "", resourceFile, "").bufferedWriter().use { writer ->
-                            for (service in allServices) {
-                                writer.write(service)
+                            for (configuration in autoConfigurationImpls) {
+                                writer.write(configuration)
                                 writer.newLine()
                             }
                         }
-                        logger.info("Wrote to: $resourceFile")
+                        logger.info(supportAnnotation() + " wrote to: $resourceFile")
                     } catch (e: IOException) {
-                        logger.error("Unable to create $resourceFile, $e")
+                        logger.error(supportAnnotation() + " unable to create $resourceFile, $e")
                     }
                 }
                 providers.clear()
