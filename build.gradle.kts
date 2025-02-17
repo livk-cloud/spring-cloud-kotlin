286a24a5ee3c7bb3073f605d8f03aba61ce31b24
livk <livk.cloud@gmail.com>
2025-02-17 16:23:00 +0800

    feat: 仓库调整

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,6 @@ configure(gradleModuleProjects) {
 
 allprojects {
     repositories {
-        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+        maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
     }
 }
